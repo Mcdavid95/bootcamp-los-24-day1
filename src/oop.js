83e7e288200a4ab7dcbd7549b29a6dd185fe5ccc
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-07-05 14:31:58 +0100

    (Feedback): - Applied Inheritance, Encapsulation, and Polymorphism

--- src/oop.js
+++ src/oop.js
@@ -1,3 +1,4 @@
+
 class Account  {
     /**
      * @description {defines an details perculiar to each account}
@@ -6,11 +7,12 @@ class Account  {
      * @param {number} balance 
      * @param {number} creditLine 
      */
-    constructor(holder, acctNumber, balance, creditLine = 10000) {
-        this.Holder = holder;
-        this.Number = acctNumber;
-        this.Balance = balance;
-        this.Creditline = creditLine;
+    constructor(holder, acctNumber, balance, year = new Date().getFullYear(), creditLine) {
+        this._Holder = holder;
+        this._Number = acctNumber;
+        this._Balance = balance;
+        this._Creditline = creditLine;
+        this._Year = year;
     }
     /**
      * @description {amount increments value of Balance}
@@ -25,20 +27,23 @@ class Account  {
      * @return {boolean} 
      */
     withdraw(amount) {
-        if (this.Balance - amount < this.Creditline) {
+        if (this._Balance - amount < this._Creditline) {
             console.log("Insufficient Balance");
             return false;
         } else {
-            this.Balance -= amount;
+            this._Balance -= amount;
             return true;
         }
+    }
+    getYear() {
+        return this._Year
     }
     /**
      * @description {returns the current balance in the account}
      * @return {number}
      */
     balance() {
-        return this.Balance;
+        return this._Balance;
     }
     /**
      * 
@@ -46,13 +51,50 @@ class Account  {
      * @param {number} amount 
      */
     transfer(target, amount) {
-        if (this.Balance - amount < this.Creditline) {
+        if (this._Balance - amount < this._Creditline) {
             console.log("Coverage Insufficient")
             return false;
         } else {
-            this.Balance -= amount;
-            target.Balance += amount;
+            this._Balance -= amount;
+            target._Balance += amount;
             return true
+        }
+    }
+}
+
+class SavingsAccount extends Account {
+     constructor(holder, acctNumber, balance, year, creditLine = 1000){
+         super(holder, acctNumber, balance, year, creditLine);
+     }
+}
+
+class CurrentAccount extends Account {
+    constructor(holder, acctNumber, balance, year, creditLine = 5000) {
+        super(holder, acctNumber, balance, year, creditLine)
+    }
+}
+
+class FixedDeposit extends Account {
+    constructor(holder, acctNumber, balance, year, creditLine = 100000) {
+        super(holder, acctNumber, balance, year, creditLine);
+    }
+    getStartYear() {
+        if (new Date().getFullYear() < this.getYear() + 10) {
+            return false
+        }
+        return true;
+    }
+    transfer(target, amount) {
+        if(getStartYear() = true) {
+            if (this._Balance - amount < this._Creditline) {
+                return "Coverage Insufficient";
+            } else {
+                this._Balance -= amount;
+                target._Balance += amount;
+                return true
+            }
+        } else {
+            return "Account is still frozen";
         }
     }
 }
